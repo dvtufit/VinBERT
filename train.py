1c89dfe366b804f160e10afb40347934adac6788
Do Van Tu <Dotu30257@gmail.com>
2024-10-17 00:47:23 -0700

    update: train.py add dataset

--- train.py
+++ train.py
@@ -1,59 +1,41 @@
-from configuration_internvl_chat import InternVLChatConfig
-from transformers import AutoModel, AutoConfig
+import torch
+from transformers import AutoModel, AutoConfig, AutoTokenizer
 from vin_bert_modeling import BERTInternVLChatModel
 from peft import LoraConfig, get_peft_model
-
-def freeze_model_org(vin_bert):
-    for param in vin_bert.parameters():
-        param.requires_grad = False
-
-    total_model_params_org = sum(p.numel() for p in vin_bert.parameters())
-    
-    print(f'Total parameter in vin_bert: {total_model_params_org//1000000} M')
-
-    total_trainable_params_org = sum(p.numel() for p in vin_bert.parameters() if p.requires_grad)
-
-    print(f'Total trainable parameter in vin_bert: {total_trainable_params_org/1000000} M')
-
-
-
-def unfreeze_model_lora(vin_bert_with_lora):  
-    for param in vin_bert_with_lora.mlp1.parameters():  
-        param.requires_grad = True
-
-    for param in vin_bert_with_lora.phobert.pooler.parameters():  
-        param.requires_grad = True
-    
-    for param in vin_bert_with_lora.phobert.mlp.parameters():  
-        param.requires_grad = True
-
-    for param in vin_bert_with_lora.phobert.classifier.parameters():  
-        param.requires_grad = True
-             
-    
-    lora_params = []
-
-    for name, param in vin_bert_with_lora.named_parameters():
-        if "lora" in name:  
-            lora_params.append(param)
-
-    total_lora_params = sum(p.numel() for p in lora_params)
-
-    print(f'Total trainable of LoRA: {total_lora_params/1000000}')
-    
-    total_trainable_params_org = sum(p.numel() for p in vin_bert_with_lora.parameters() if p.requires_grad)
-    
-    print(f'Total trainable parameter in vin_bert_with_lora: {total_trainable_params_org/1000000} M')
+from utils import freeze_model_org, unfreeze_model_lora
+from vin_bert_dataset import UITDataset, PromptTemplate, ImageProcessor
+from torch.utils.data import DataLoader
 
 
 if __name__ == '__main__':
+    train_image_dir = "data/train-images"
+    train_text_path = "data/vimmsd-train.json"
+
+    tokenizer = AutoTokenizer.from_pretrained("5CD-AI/Vintern-1B-v2", trust_remote_code=True, use_fast=False)
+    template = PromptTemplate()
+    image_processor = ImageProcessor()
+    dataset = UITDataset(train_image_dir, train_text_path, tokenizer, template, image_processor)
+
+    train_size = int(0.7 * len(dataset)) 
+    test_size = len(dataset) - train_size 
+
+    val_size = int(0.5 * test_size) 
+    test_size = test_size - val_size 
+
+    train_dataset,  test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size + val_size])
+    val_dataset, test_dataset = torch.utils.data.random_split(test_dataset, [val_size, test_size])
+
+    train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)  
+    val_dataloader = DataLoader(val_dataset, batch_size=2, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=2, shuffle=False) 
+
+
     config = AutoConfig.from_pretrained(
         "5CD-AI/Vintern-1B-v2" ,
         trust_remote_code=True, 
     )
     model = AutoModel.from_pretrained(
         "5CD-AI/Vintern-1B-v2", 
-    #     torch_dtype=torch.float16,
         low_cpu_mem_usage=True,
         trust_remote_code=True,
     ).eval().cuda()
@@ -67,7 +49,6 @@ if __name__ == '__main__':
     vin_bert.img_context_token_id = 151648
 
     vin_bert = vin_bert.cuda()
-
     freeze_model_org(vin_bert)
 
     lora_config = LoraConfig(
@@ -76,17 +57,16 @@ if __name__ == '__main__':
         lora_dropout=0.1,
         bias="none",
         target_modules=[
-            "q_proj", "k_proj", "v_proj", "o_proj",# Attention modules trong llm
+            "q_proj", "k_proj", "v_proj", "o_proj",
             "patch_embedding",
             "language_model.embed_tokens" 
-            "self.query",                         # Phobert Attention
+            "self.query",                         
             "self.key",
             "self.value",
-            "output.dense"                        # Phobert MLP
+            "output.dense"                        
         ]
     )
 
     vin_bert_with_lora = get_peft_model(vin_bert, lora_config)
-
     unfreeze_model_lora(vin_bert_with_lora)
 
